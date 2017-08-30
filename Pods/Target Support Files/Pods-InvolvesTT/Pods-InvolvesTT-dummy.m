7f71e051b739c69f749e4d0af5ed6298419a551e
Rafael Leandro <rafaelleandro@me.com>
2017-08-29 23:51:35 -0300

    delete old files

--- Pods/Target Support Files/Pods-InvolvesTT/Pods-InvolvesTT-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_InvolvesTT : NSObject
-@end
-@implementation PodsDummy_Pods_InvolvesTT
-@end
--- Pods/Target Support Files/Pods-InvolvesTTTests/Pods-InvolvesTTTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_InvolvesTTTests : NSObject
-@end
-@implementation PodsDummy_Pods_InvolvesTTTests
-@end
